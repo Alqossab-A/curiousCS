7cc36a4c5513151ca42906bc13ab9f6a2a71e003
Ahmed <alqossaba@gmail.com>
2024-01-07 22:47:56 -0500

    learning vue

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script>
+    export default {
+        data() {
+            return {
+                count: 0,
+            };
+        },
+        methods: {
+            incrementCount() {
+                this.count++;
+            }
+        },
+    };
+</script>
+
+<template>
+    <div>
+        {{ count }}
+    </div>
+    <button @click='incrementCount'>
+        ++
+    </button>
+    <div>
+        main body
+    </div>
+    <div>
+        footer
+    </div>
+</template>
+
